7832eb6e21eb75f828b4f3766cbc5767912bb185
Carlos de Alfonso <caralla@upv.es>
2015-12-10 09:36:50 +0100

    enhancement of rest utils

--- restutils.py
+++ restutils.py
@@ -35,7 +35,14 @@ def get_app():
 
 # Declaration of new class that inherits from ServerAdapter  
 # It's almost equal to the supported cherrypy class CherryPyServer  
-class MySSLCherryPy(bottle.ServerAdapter):  
+class MySSLCherryPy(bottle.ServerAdapter):
+    def __init__(self, ssl_certfile = None, ssl_keyfile = None, ssl_ca_certs = None, host='127.0.0.1', port=8080, **options):
+        self._ssl_certfile = ssl_certfile
+        self._ssl_keyfile = ssl_keyfile
+        self._ssl_ca_certs = ssl_ca_certs
+        bottle.ServerAdapter.__init__(self, host, port, options)
+        
+    
     def run(self, handler):
         from cherrypy.wsgiserver.ssl_builtin import BuiltinSSLAdapter
         from cherrypy import wsgiserver
@@ -44,7 +51,7 @@ class MySSLCherryPy(bottle.ServerAdapter):
         
         # If cert variable is has a valid path, SSL will be used  
         # You can set it to None to disable SSL
-        server.ssl_adapter = BuiltinSSLAdapter(Config.REST_SSL_CERTFILE, Config.REST_SSL_KEYFILE, Config.REST_SSL_CA_CERTS)
+        server.ssl_adapter = BuiltinSSLAdapter(self._ssl_certfile, self._ssl_keyfile, self._ssl_ca_certs)
         try:  
             server.start()  
         finally:  
@@ -82,18 +89,21 @@ class StoppableWSGIRefServer(bottle.ServerAdapter):
         self.srv.shutdown()
         # self.server.server_close()
 
-def run_in_thread(host, port, use_ssl = False):
-    _bottle_thr = threading.Thread(target=run, args=(host, port, use_ssl))
+def run_in_thread(host, port, use_ssl = False, ssl_certfile = None, ssl_keyfile = None, ssl_ca_certs = None):
+    _bottle_thr = threading.Thread(target=run, args=(host, port, use_ssl, ssl_certfile, ssl_keyfile, ssl_ca_certs))
     _bottle_thr.daemon = True
     _bottle_thr.start()
     return _bottle_thr
 
-def run(host, port, use_ssl = False):
+def run(host, port, use_ssl = False, ssl_certfile = None, ssl_keyfile = None, ssl_ca_certs = None):
     global _bottle_server, _app
     if use_ssl:
         # Add our new MySSLCherryPy class to the supported servers  
         # under the key 'mysslcherrypy'
-        _bottle_server = MySSLCherryPy(host=host, port=port)
+        if use_ssl and (ssl_certfile is None or ssl_keyfile is None or ssl_ca_certs is None):
+            raise Exception("You must provide certificate, key file and the CA path to enable the SSL server")
+        
+        _bottle_server = MySSLCherryPy(ssl_certfile, ssl_keyfile, ssl_ca_certs, host=host, port=port)
         bottle.run(_app, host=host, port=port, server=_bottle_server, quiet=True)
     else:
         _bottle_server = StoppableWSGIRefServer(host=host, port=port)
