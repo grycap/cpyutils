38c8a8a8364e5848739f0f0ad37aa9fca40c26ce
Carlos de Alfonso <caralla@upv.es>
2015-12-10 09:38:12 +0100

    version bump

--- version.py
+++ version.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # 
-VERSION="0.10"
+VERSION="0.11"
 
 def get():
     global VERSION
